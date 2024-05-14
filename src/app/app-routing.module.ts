ea1ea1c752a009c572da2c3fd133b1a50609c076
amal <amalbenothmen9@gmail.com>
2024-05-14 14:50:37 +0100

    final version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter } from '@angular/router';
+import { IndexComponent } from './post/index/index.component';
+import { ViewComponent } from './post/view/view.component';
+import { CreateComponent } from './post/create/create.component';
+import { EditComponent } from './post/edit/edit.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from'@angular/platform-browser/animations';
 
-const routes: Routes = [];
+const routes: Routes = [ { path: 'post', redirectTo: 'post/index',
+pathMatch: 'full'},
+{ path: 'post/index', component: IndexComponent },
+{ path: 'post/:postId/view', component:ViewComponent },
+{ path: 'post/create', component: CreateComponent},
+{ path: 'post/:postId/edit', component:EditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  providers: [provideRouter(routes),
+    provideAnimations(),
+     provideHttpClient()],
+  exports: [RouterModule],
+ 
 })
 export class AppRoutingModule { }
